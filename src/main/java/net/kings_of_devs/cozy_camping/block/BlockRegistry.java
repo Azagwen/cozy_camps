44298e304913be2abedace68220ad14251a3015e
QuimTorra <q.torra@gmail.com>
2021-08-23 10:21:29 +0200

    Made the Charcoal Block be a Fuel, idk if that's the correct workflow, but I wanted to add something xD

--- src/main/java/net/kings_of_devs/cozy_camping/block/BlockRegistry.java
+++ src/main/java/net/kings_of_devs/cozy_camping/block/BlockRegistry.java
@@ -1,6 +1,7 @@
 package net.kings_of_devs.cozy_camping.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.kings_of_devs.cozy_camping.CozyCampingMain;
 import net.minecraft.block.Block;
@@ -11,5 +12,6 @@ public class BlockRegistry {
 
     public static void init() {
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "charcoal_block", CHARCOAL_BLOCK);
+        FuelRegistry.INSTANCE.add(CHARCOAL_BLOCK, 16000); //this makes the charcoal block be a fuel, with the same duration as a normal coal block.
     }
 }
