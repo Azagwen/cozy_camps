d2d7ce63c495351696679d83ecf7d900df720b21
QuimTorra <q.torra@gmail.com>
2021-08-25 13:31:35 +0200

    Added a folder for entity textures

--- src/main/java/net/kings_of_devs/cozy_camping/item/ItemRegistry.java
+++ src/main/java/net/kings_of_devs/cozy_camping/item/ItemRegistry.java
@@ -1,6 +1,7 @@
 package net.kings_of_devs.cozy_camping.item;
 
 import net.kings_of_devs.cozy_camping.CozyCampingMain;
+import net.kings_of_devs.cozy_camping.item.walking_stick.WalkingStick;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 
@@ -11,6 +12,7 @@ public class ItemRegistry {
     public static final Item MARSHMALLOW_ON_A_STICK = new MarshmallowItem(2, new Item.Settings().food(new FoodComponent.Builder().hunger(6).saturationModifier(7f).snack().build()).maxCount(1));
     public static final Item ROASTED_MARSHMALLOW_ON_A_STICK = new MarshmallowItem(3, new Item.Settings().food(new FoodComponent.Builder().hunger(10).saturationModifier(8f).snack().build()).maxCount(1));
     public static final Item BURNED_STICK = new Item(new Item.Settings().maxCount(64));
+    public static final Item WALKING_STICK = new WalkingStick(new Item.Settings().maxCount(1));
 
     public static void init(){
         ItemRegistryUtil.registerItem(CozyCampingMain.MISC_TAB, "marshmallow", MARSHMALLOW);
@@ -18,6 +20,7 @@ public class ItemRegistry {
         ItemRegistryUtil.registerItem(CozyCampingMain.MISC_TAB, "marshmallow_on_a_stick", MARSHMALLOW_ON_A_STICK);
         ItemRegistryUtil.registerItem(CozyCampingMain.MISC_TAB, "roasted_marshmallow_on_a_stick", ROASTED_MARSHMALLOW_ON_A_STICK);
         ItemRegistryUtil.registerItem(CozyCampingMain.MISC_TAB, "burned_stick", BURNED_STICK);
+        ItemRegistryUtil.registerItem(CozyCampingMain.MISC_TAB, "walking_stick", WALKING_STICK);
 
         CozyCampingMain.LOGGER.info("Cozy Camping Items Registered!");
     }
