da33a89c6d3ae71077db2cc47b4123494e2f0987
QuimTorra <q.torra@gmail.com>
2021-08-25 15:36:52 +0200

    Camper Texture Mixin

---
+++ src/main/java/net/kings_of_devs/cozy_camping/mixin/VillagerTextureMixin.java
@@ -0,0 +1,20 @@
+package net.kings_of_devs.cozy_camping.mixin;
+
+import net.minecraft.client.render.entity.feature.VillagerClothingFeatureRenderer;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VillagerClothingFeatureRenderer.class)
+public class VillagerTextureMixin {
+    private final VillagerClothingFeatureRenderer self = (VillagerClothingFeatureRenderer) (Object) this;
+
+    @Inject(method = "findTexture", at = @At("HEAD"), cancellable = true)
+    private void camperTexture(String keyType, Identifier keyId, CallbackInfoReturnable<Identifier> cir){
+        if(keyId.getPath().equalsIgnoreCase("camper")) {
+            cir.setReturnValue(new Identifier("cozy_camping", "textures/entity/camper.png"));
+        }
+    }
+}
