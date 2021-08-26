13d0139786e393445ed224b07f8c776c726f6e5a
QuimTorra <q.torra@gmail.com>
2021-08-26 12:36:51 +0200

    More Traps progress (needs new model + texture)

--- src/main/java/net/kings_of_devs/cozy_camping/block/TrapBlock.java
+++ src/main/java/net/kings_of_devs/cozy_camping/block/TrapBlock.java
@@ -1,6 +1,7 @@
 package net.kings_of_devs.cozy_camping.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.kings_of_devs.cozy_camping.mixin.LivingEntityMixin;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
@@ -23,15 +24,18 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.event.GameEvent;
 
 public class TrapBlock extends Block {
-
     public static final BooleanProperty CLOSED = BooleanProperty.of("closed");
 
     public TrapBlock(Settings settings) {
@@ -60,16 +64,45 @@ public class TrapBlock extends Block {
             if(entity instanceof LivingEntity trappedEntity){
                 trappedEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20, 255, true, false));
                 trappedEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 20, 1, true, false));
+                ((LivingEntityMixin) trappedEntity).setJumpingCooldown(60); //this prevents players from jumping when trapped
                 trappedEntity.damage(DamageSource.MAGIC, 1);
             }
         }
     }
 
-    /*@Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        world.setBlockState(pos, state.with(CLOSED, !world.getBlockState(pos).get(CLOSED)));
-        player.playSound(SoundEvents.BLOCK_ANVIL_BREAK, 1, 1);
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return direction == Direction.DOWN && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
 
-        return ActionResult.SUCCESS;
-    }*/
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return sideCoversSmallSquare(world, pos.down(), Direction.UP);
+    }
+
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return world.getBlockState(pos).get(CLOSED) && Direction.UP != direction ? 15 : 0;
+    }
+
+    public int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return direction == Direction.DOWN ? state.getWeakRedstonePower(world, pos, direction) : 0;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(state.get(CLOSED)) {
+            world.setBlockState(pos, state.with(CLOSED, false));
+            player.playSound(SoundEvents.BLOCK_ANVIL_BREAK, 1, 1);
+
+            return ActionResult.SUCCESS;
+
+        }
+        return ActionResult.FAIL;
+
+    }
+
+
 }
