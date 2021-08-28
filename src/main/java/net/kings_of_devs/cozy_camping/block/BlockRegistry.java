0a3287b312038b6dc0ebe2b511e67f0a3490a100
Azagwen <30528331+Azagwen@users.noreply.github.com>
2021-08-28 11:38:12 +0200

    refactors

--- src/main/java/net/kings_of_devs/cozy_camping/block/BlockRegistry.java
+++ src/main/java/net/kings_of_devs/cozy_camping/block/BlockRegistry.java
@@ -7,9 +7,12 @@ import net.minecraft.block.*;
 
 public class BlockRegistry {
 
+    public static final Block CHARCOAL_BLOCK = new Block(FabricBlockSettings.copyOf(Blocks.COAL_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES));
+
     public static final Block TENT = new TentBlock(FabricBlockSettings.copyOf(Blocks.WHITE_WOOL).nonOpaque());
     public static final Block RIPPED_TENT = new TentBlock(FabricBlockSettings.copyOf(Blocks.WHITE_WOOL).nonOpaque());
     public static final Block FANCY_TENT = new TentBlock(FabricBlockSettings.copyOf(Blocks.WHITE_WOOL).nonOpaque());
+
     public static final Block OAK_STUMP = new StumpBlock(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS).breakByTool(FabricToolTags.AXES));
     public static final Block SPRUCE_STUMP = new StumpBlock(FabricBlockSettings.copyOf(Blocks.SPRUCE_PLANKS).breakByTool(FabricToolTags.AXES));
     public static final Block BIRCH_STUMP = new StumpBlock(FabricBlockSettings.copyOf(Blocks.BIRCH_PLANKS).breakByTool(FabricToolTags.AXES));
@@ -18,7 +21,7 @@ public class BlockRegistry {
     public static final Block DARK_OAK_STUMP = new StumpBlock(FabricBlockSettings.copyOf(Blocks.DARK_OAK_PLANKS).breakByTool(FabricToolTags.AXES));
     public static final Block CRIMSON_STUMP = new StumpBlock(FabricBlockSettings.copyOf(Blocks.CRIMSON_PLANKS).breakByTool(FabricToolTags.AXES));
     public static final Block WARPED_STUMP = new StumpBlock(FabricBlockSettings.copyOf(Blocks.WARPED_PLANKS).breakByTool(FabricToolTags.AXES));
-    public static final Block CHARCOAL_BLOCK = new Block(FabricBlockSettings.copyOf(Blocks.COAL_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES));
+
     public static final Block BEAR_TRAP = new TrapBlock(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES).nonOpaque());
 
     public static final Block BOULDER = new BoulderBlock(FabricBlockSettings.copyOf(Blocks.STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES).nonOpaque());
@@ -31,9 +34,12 @@ public class BlockRegistry {
     public static final Block HEATHER = new HeatherBlock(FabricBlockSettings.copyOf(Blocks.GRASS));
 
     public static void init() {
+        BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "charcoal_block", CHARCOAL_BLOCK);
+
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "tent", TENT);
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "ripped_tent", RIPPED_TENT);
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "fancy_tent", FANCY_TENT);
+
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "oak_stump", OAK_STUMP);
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "spruce_stump", SPRUCE_STUMP);
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "birch_stump", BIRCH_STUMP);
@@ -42,7 +48,7 @@ public class BlockRegistry {
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "dark_oak_stump", DARK_OAK_STUMP);
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "crimson_stump", CRIMSON_STUMP);
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "warped_stump", WARPED_STUMP);
-        BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "charcoal_block", CHARCOAL_BLOCK);
+
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "bear_trap", BEAR_TRAP);
 
         BlockRegistryUtil.registerBlock(CozyCampingMain.BLOCKS_TAB, "stone_boulder", BOULDER);
